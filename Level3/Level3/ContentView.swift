c6889b84525898dc08cfa4ef09f747500c115e9f
보경 <97583162+bokoo14@users.noreply.github.com>
2023-03-25 22:56:39 +0900

    feat: build project

---
+++ Level3/Level3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Level3
+//
+//  Created by Bokyung on 2023/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Level3/Level3/Level3App.swift
@@ -0,0 +1,17 @@
+//
+//  Level3App.swift
+//  Level3
+//
+//  Created by Bokyung on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Level3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
