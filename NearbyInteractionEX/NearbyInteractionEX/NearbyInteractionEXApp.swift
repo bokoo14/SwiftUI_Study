8a6d5679db7f6bf4e1db8eeebe634b81da2395de
보경 <97583162+bokoo14@users.noreply.github.com>
2023-05-02 21:09:12 +0900

    feat: multipeer connectivity

---
+++ NearbyInteractionEX/NearbyInteractionEX/NearbyInteractionEXApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyInteractionEXApp.swift
+//  NearbyInteractionEX
+//
+//  Created by Bokyung on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct NearbyInteractionEXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
