575fc2853369b7be788de0bed0e8df34404cc5da
보경 <97583162+bokoo14@users.noreply.github.com>
2023-06-05 20:25:21 +0900

    feat: settingsPeople 뷰 - ScrollView

--- Chulsseuks/Chulsseuks/View/Settings/SettingsPeopleView.swift
+++ Chulsseuks/Chulsseuks/View/Settings/SettingsPeopleView.swift
@@ -9,12 +9,39 @@ import SwiftUI
 
 struct SettingsPeopleView: View {
     var body: some View {
-        Text("Settings People View")
+        ScrollView {
+            HStack{
+                Image("character")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150)
+                    .padding(10)
+                    .background(.yellow)
+                    .clipShape(Circle()) // 이미지 둥글게 자르기
+                    
+                VStack (alignment: .leading){
+                    Text("루나 / Luna")
+                        .foregroundColor(.indigo)
+                        .font(.system(size: 20))
+                    Text("iOS Developer")
+                        .foregroundColor(.gray)
+                    Text("무한한 성장을 원하는 개발자")
+                }
+                Spacer()
+            }// :HStack - 1명의 사람
+            
+                .navigationTitle("출쓱을 만든 사람들")
+                .navigationBarTitleDisplayMode(.inline)
+        } //: ScrollView
+        
     }
 }
 
 struct SettingsPeopleView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsPeopleView()
+        NavigationView {
+            SettingsPeopleView()
+        }
+        
     }
 }
