79c4fa9a1c2d7fcd0182886520d9c9511436934d
보경 <97583162+bokoo14@users.noreply.github.com>
2023-06-08 11:38:09 +0900

    feat: rotation3DEffect - degree 설정

--- Chulsseuks/Chulsseuks/View/MainVIew/FlashCardView.swift
+++ Chulsseuks/Chulsseuks/View/MainVIew/FlashCardView.swift
@@ -16,32 +16,61 @@ struct FlashCardView<Front, Back>: View  where Front: View, Back: View {
     var back: () -> Back
     
     @State var flipped: Bool = false
-    @State var flashcardRotation = 0.0 // 플래시 카드 뷰 전체의 회전을 제어
     @State var contentRotation = 0.0 // 플래시 카드 내용의 회전을 제어
+    @State var flashcardRotation = 0.0 // 플래시 카드 뷰 전체의 회전을 제어
     
     var body: some View {
         ZStack{
+            // flppped가 true이면 back View를 보여줌, false이면 front View를 보여줌
             if flipped {
                 back()
             }
-            else  {
+            else {
                 front()
             }
         } //: ZStack
         // card를 y축으로 3D로 돌리는 effect
-        .rotation3DEffect(.degrees(contentRotation), axis: (x: 0, y: 1, z: 0))
+        .rotation3DEffect(.degrees(contentRotation), axis: (x: 0, y: 1, z: 0)) // 카드 내용을 제어
+        .rotation3DEffect(.degrees(flashcardRotation), axis: (x: 0, y: 1, z: 0), perspective: 0.2) // 플래시 카드 전체를 제어
         .frame(height: 463)
         .frame(maxWidth: .infinity)
-        
-    }
+        .onTapGesture {
+            flipped ? flipBackFlashedCard() : flipFlashedCard()
+        }
+    } // : body: View
     
-    // function
+    
+    // func
+    // 카드 View가 front일때 -> Card를 y축을 기준으로 시계 반대방향으로 회전
     func flipFlashedCard() {
+        let animationTime: Double = 0.5
+        // 카드 자체를 flip - duration: 0.5초
+        withAnimation(Animation.linear(duration: animationTime)) {
+            contentRotation -= 180 //
+        }
         
+        // 카드 내의 컴포넌트들을 flip - duration을 엄청 작게 만들어서 마지막의 rotation 효과만 보여주게 함
+        // duration을 0.001로 한 이유: 원래는 1바퀴 돌고 마지막에 180도 돌면서 멈추는 구간이 느리게 보이는데, duration을 엄청 작게 주면 1바퀴 도는 부분이 잘 안보이게 됨
+        withAnimation(Animation.linear(duration: 0.001).delay(animationTime / 2)) {
+            contentRotation -= 180
+            flipped.toggle()
+        }
     }
     
+    // 카드 View가 back일때 -> Card를 y축을 기준으로 시계방향으로 회전
     func flipBackFlashedCard() {
+        let animationTime: Double = 0.5
+        // 카드 자체를 flip - duration: 0.5초
+        withAnimation(Animation.linear(duration: animationTime)) {
+            contentRotation += 180 //
+        }
         
+        // 카드 내의 컴포넌트들을 flip - duration을 엄청 작게 만들어서 마지막의 rotation 효과만 보여주게 함
+        // duration을 0.001로 한 이유: 원래는 1바퀴 돌고 마지막에 180도 돌면서 멈추는 구간이 느리게 보이는데, duration을 엄청 작게 주면 1바퀴 도는 부분이 잘 안보이게 됨
+        withAnimation(Animation.linear(duration: 0.001).delay(animationTime / 2)) {
+            contentRotation += 180
+            flipped.toggle()
+        }
     }
     
 }
