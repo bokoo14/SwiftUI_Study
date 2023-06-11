2dedab2f2696beb943627735e98c3f33cc5d5ac0
보경 <97583162+bokoo14@users.noreply.github.com>
2023-06-11 21:59:13 +0900

    refactor: StringData분리

---
+++ Chulsseuks/Chulsseuks/Model/SrtingData.swift
@@ -0,0 +1,30 @@
+//
+//  SrtingData.swift
+//  Chulsseuks
+//
+//  Created by Bokyung on 2023/06/11.
+//
+
+import Foundation
+
+// MARK: 각각의 String Data를 구조화
+struct stateComponentString {
+    let avsence = "결석"
+    let late = "지각"
+    let attendance = "출석"
+}
+
+struct MainButtonString {
+    let mainBtn = "출쓱하기"
+}
+
+struct CalendarViewString {
+    let log = "출쓱기록"
+}
+
+struct CardFrontViewString {
+    let image = "character"
+    let text1 = "아래 버튼을 눌러\n 오늘의 출쓱을 진행해주세요."
+    let text2 = "출석 규정 보러가기 >"
+}
+
