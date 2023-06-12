bf6d49135992e783efac18fec8ca9f06a65616db
보경 <97583162+bokoo14@users.noreply.github.com>
2023-06-12 23:28:27 +0900

    refactor: 불필요한 주석 제거

--- Chulsseuks/Chulsseuks/View/MainVIew/FightingView.swift
+++ Chulsseuks/Chulsseuks/View/MainVIew/FightingView.swift
@@ -13,7 +13,6 @@ struct FightingView: View {
     var body: some View {
         HStack {
             Text("월클 디벨로퍼 \(nickname).\n오늘도 화이팅하세요!")
-            //.foregroundColor(.white)
                 .font(.system(size: 20, weight: .regular))
                 .lineSpacing(5)
             Spacer()
