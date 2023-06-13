37607cc98ce9310200dd04c0381a7de22bc83962
보경 <97583162+bokoo14@users.noreply.github.com>
2023-06-13 16:59:09 +0900

    feat: model, viewmodel로 swipeview 생성

--- AppleStores/AppleStores/View/Swipe/SwipePromotionView.swift
+++ AppleStores/AppleStores/View/Swipe/SwipePromotionView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct SwipePromotionView: View {
+    // ViewModel을 초기화할 떄는 @StateObject로 불러오기
+    @StateObject var swipeViewModel: SwipeViewModel = SwipeViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack (spacing: 0) {
+            VStack {
+                Text("\(Image(systemName: swipeViewModel.swipePro[0].imageName))")
+                    .font(.system(size: 36))
+                    .border(.red)
+            }
+            .padding(EdgeInsets(top: 20, leading: 20, bottom: 75, trailing: 12))
+            .border(.red)
+            
+            VStack (alignment: .leading, spacing: 0) {
+                Text(swipeViewModel.swipePro[0].title)
+                    .font(.system(size: 13, weight: .medium))
+                    .border(.green)
+                Text(swipeViewModel.swipePro[0].explain)
+                    .font(.system(size: 12, weight: .light))
+                    .border(.green)
+                Text(swipeViewModel.swipePro[0].viewMore)
+                    .font(.system(size: 12, weight: .medium))
+                    .foregroundColor(Color("ColorFontBlue"))
+                    .border(.green)
+            }
+            .frame(maxWidth: 274)
+            .border(.green)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .border(.blue)
+        
     }
 }
 
--- AppleStores/AppleStores/ViewModel/SwipeViewModel.swift
+++ AppleStores/AppleStores/ViewModel/SwipeViewModel.swift
@@ -9,8 +9,9 @@ import Foundation
 
 // ViewModel은 Class로 선언해야 함
 class SwipeViewModel: ObservableObject {
-    @Published var swipeOne: [SwipeModel] = []
+    @Published var swipePro: [SwipeModel] = []
     
+    // ViewModel이 초기화될 때마다 실행될 함수
     init() {
         getData()
     }
@@ -20,6 +21,8 @@ class SwipeViewModel: ObservableObject {
         let swipeTwo = SwipeModel(imageName: "macbook.and.iphone", title: "Apple Trade In", explain: "iPhoen 8 이후 모델을 보상 판매하면 iPhone14 또는 iPhoe 14 Pro 구입 시 사용할 수 있는 ￦40,000 ~ ￦780,000원 상당의 크리딧이. ", viewMore: "더 알아보기")
         let swipeThree = SwipeModel(imageName: "airpodspro.chargingcase.wireless", title: "Airpod Pro", explain: "에어팟 프로를 잃어버렸습니다. 제발 제 에어팟 프로를 찾아주세요. 찾아주신 분에겐 ￦20,000 상당의 크레딧이. *", viewMore: "더 알아보기")
         
-        self.swipeOne.append(swipeOne)
+        self.swipePro.append(swipeOne)
+        self.swipePro.append(swipeTwo)
+        self.swipePro.append(swipeThree)
     }
 }
