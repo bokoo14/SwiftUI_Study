37c5629d65f37b0eaea638186ae73000dffacba1
보경 <97583162+bokoo14@users.noreply.github.com>
2023-06-15 18:44:25 +0900

    feat: recommed - card view 생성

---
+++ AppleStores/AppleStores/View/Recommend/CardView.swift
@@ -0,0 +1,22 @@
+//
+//  CardView.swift
+//  AppleStores
+//
+//  Created by Bokyung on 2023/06/15.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
