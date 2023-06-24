f9e607ecaa655a10fb54a9157afc171a54667c6a
보경 <97583162+bokoo14@users.noreply.github.com>
2023-06-24 13:39:09 +0900

    make new project

---
+++ AppleStore2/AppleStore2/AppleStore2App.swift
@@ -0,0 +1,17 @@
+//
+//  AppleStore2App.swift
+//  AppleStore2
+//
+//  Created by Bokyung on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleStore2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
