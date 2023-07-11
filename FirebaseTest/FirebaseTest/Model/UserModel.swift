3dadfcc20c735bd8db0e668126d386244f3dbfc7
보경 <97583162+bokoo14@users.noreply.github.com>
2023-07-12 00:05:22 +0900

    feat: UserModel 생성

--- FirebaseTest/FirebaseTest/Model/UserModel.swift
+++ FirebaseTest/FirebaseTest/Model/UserModel.swift
@@ -8,3 +8,21 @@
 import Foundation
 
 // 16명의 user에 대한 Data를 Model에 저장
+
+//예시: 유저1
+//    - userName :  “Luna”
+//    - userID: 1
+//    - gender : 여자/ female
+//    - email : dsfasfd@naver.com
+//    - group: 1
+struct UserModel: Identifiable {
+    var id: UUID = UUID()
+    var userName: String
+    var userID: Int
+    var gender: String
+    var email: String
+    var group: Int
+}
+
+
+
--- FirebaseTest/FirebaseTest/View/MakeGroupView.swift
+++ FirebaseTest/FirebaseTest/View/MakeGroupView.swift
@@ -33,23 +33,23 @@ struct MakeGroupView: View {
                     }
                 }
                 
-
+                
                 // 단일 문서를 만들거나 덮어쓰려면 다음 언어별 set() 메서드를 사용합니다.
                 // Add a new document in collection "cities"
-                firebaseDB.collection("user").document("R5Vbave5WZlo8D7PD0Os").setData([
-                    "usrName": "Luna",
-                    "userID": "1",
-                    "gender": "female",
-                    "email": "asdfssdf@naver.com",
-                    "group": 1,
-                    "sentHeart": "Heemoo"
-                ]) { err in
-                    if let err = err {
-                        print("Error writing document: \(err)")
-                    } else {
-                        print("Document successfully written!")
-                    }
-                }
+                //                firebaseDB.collection("user").document("R5Vbave5WZlo8D7PD0Os").setData([
+                //                    "usrName": "Luna",
+                //                    "userID": "1",
+                //                    "gender": "female",
+                //                    "email": "asdfssdf@naver.com",
+                //                    "group": 1,
+                //                    "sentHeart": "Heemoo"
+                //                ]) { err in
+                //                    if let err = err {
+                //                        print("Error writing document: \(err)")
+                //                    } else {
+                //                        print("Document successfully written!")
+                //                    }
+                //                }
             } label: {
                 Text("사용자 데이터 추가")
             } // button
