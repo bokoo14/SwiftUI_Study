ac1b48b3a5983139e55ee40b1f849a3a49c98a6b
보경 <97583162+bokoo14@users.noreply.github.com>
2023-07-12 10:06:25 +0900

    fix: forEach문으로 오류 수정

--- FirebaseTest/FirebaseTest/Model/UserModel.swift
+++ FirebaseTest/FirebaseTest/Model/UserModel.swift
@@ -22,7 +22,28 @@ struct UserModel: Identifiable {
     var gender: String
     var email: String
     var group: Int
+    var target: String
 }
 
-
-
+var users: [UserModel] = [
+    // Group1
+    UserModel(userName: "Luna", userID: 1, gender: "female", email: "dsfasfd@naver.com", group: 1, target: ""),
+    UserModel(userName: "Heemoo", userID: 2, gender: "female", email: "sdfsd@naver.com", group: 1, target: ""),
+    UserModel(userName: "Puppy", userID: 3, gender: "male", email: "asdf@naver.com", group: 1, target: ""),
+    UserModel(userName: "Kiwi", userID: 4, gender: "male", email: "qweq@naver.com", group: 1, target: ""),
+    // Group2
+    UserModel(userName: "Lunaa", userID: 5, gender: "female", email: "sdfsd@naver.com", group: 2, target: ""),
+    UserModel(userName: "HeeHee", userID: 6, gender: "female", email: "qweqw@naver.com", group: 2, target: ""),
+    UserModel(userName: "Ocean", userID: 7, gender: "male", email: "ghj@naver.com", group: 2, target: ""),
+    UserModel(userName: "Goggins", userID: 8, gender: "male", email: "elrkps@naver.com", group: 2, target: ""),
+    // Group3
+    UserModel(userName: "LuLu", userID: 9, gender: "female", email: "sdlfks@naver.com", group: 3, target: ""),
+    UserModel(userName: "Hee", userID: 10, gender: "female", email: "asla@naver.com", group: 3, target: ""),
+    UserModel(userName: "Suheon", userID: 11, gender: "male", email: "qwleq@naver.com", group: 3, target: ""),
+    UserModel(userName: "Hoon", userID: 12, gender: "male", email: "poask@naver.com", group: 3, target: ""),
+    // Group4
+    UserModel(userName: "Bokoo", userID: 13, gender: "female", email: "sdlal@naver.com", group: 4, target: ""),
+    UserModel(userName: "Moo", userID: 14, gender: "female", email: "relds@naver.com", group: 4, target: ""),
+    UserModel(userName: "SuuChoi", userID: 15, gender: "male", email: "sdlfxp@naver.com", group: 4, target: ""),
+    UserModel(userName: "Yongs", userID: 16, gender: "male", email: "asldk@naver.com", group: 4, target: "")
+]
--- FirebaseTest/FirebaseTest/View/MakeGroupView.swift
+++ FirebaseTest/FirebaseTest/View/MakeGroupView.swift
@@ -18,22 +18,23 @@ struct MakeGroupView: View {
                 // data 추가
                 // Cloud Firestore는 컬렉션에 저장되는 문서에 데이터를 저장합니다. 문서에 데이터를 처음 추가할 때 Cloud Firestore에서 암시적으로 컬렉션과 문서를 만듭니다. 컬렉션이나 문서를 명시적으로 만들 필요가 없습니다.
                 // 새 컬렉션과 문서를 만듦
-                firebaseDB.collection("users").addDocument(data: [
-                    "usrName": "Puppy",
-                    "userID": "2",
-                    "gender": "male",
-                    "email": "dfgsd@naver.com",
-                    "group": 1,
-                    "sentHeart": "Kiwi"
-                ]) { err in
-                    if let err = err {
-                        print("Error adding document: \(err)")
-                    } else {
-                        print("Document added with ID")
+                users.forEach { user in
+                    firebaseDB.collection("users").addDocument(data: [
+                        "usrName": "Puppy",
+                        "userID": "2",
+                        "gender": "male",
+                        "email": "dfgsd@naver.com",
+                        "group": 1,
+                        "sentHeart": "Kiwi"
+                    ]) { err in
+                        if let err = err {
+                            print("Error adding document: \(err)")
+                        } else {
+                            print("Document added with ID")
+                        }
                     }
                 }
-                
-                
+
                 // 단일 문서를 만들거나 덮어쓰려면 다음 언어별 set() 메서드를 사용합니다.
                 // Add a new document in collection "cities"
                 //                firebaseDB.collection("user").document("R5Vbave5WZlo8D7PD0Os").setData([
