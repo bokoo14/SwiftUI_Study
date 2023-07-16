405986d0c95c0b91a7144aec1c1bd5e07df592e3
보경 <97583162+bokoo14@users.noreply.github.com>
2023-07-16 20:04:02 +0900

    feat: model data수정

--- FirebaseTest/FirebaseTest/Model/UserModel.swift
+++ FirebaseTest/FirebaseTest/Model/UserModel.swift
@@ -18,32 +18,34 @@ import Foundation
 struct UserModel: Identifiable {
     var id: UUID = UUID()
     var userName: String
-    var userID: String
     var gender: String
-    var email: String
     var group: Int
-    var target: String
+    var mySignal: String
+    var isParticipate: Bool
+    //var userID: String
+    //var email: String
 }
 
 var users: [UserModel] = [
     // Group1
-    UserModel(userName: "Luna", userID: "", gender: "female", email: "dsfasfd@naver.com", group: 1, target: ""),
-    UserModel(userName: "Heemoo", userID: "", gender: "female", email: "sdfsd@naver.com", group: 1, target: ""),
-    UserModel(userName: "Puppy", userID: "", gender: "male", email: "asdf@naver.com", group: 1, target: ""),
-    UserModel(userName: "Kiwi", userID: "", gender: "male", email: "qweq@naver.com", group: 1, target: ""),
+    UserModel(userName: "Luna",  gender: "female", group: 1, mySignal: "", isParticipate: false),
+    UserModel(userName: "Heemoo",  gender: "female", group: 1, mySignal: "", isParticipate: false),
+    UserModel(userName: "Puppy",  gender: "male", group: 1, mySignal: "", isParticipate: false),
+    UserModel(userName: "Kiwi",  gender: "male", group: 1, mySignal: "", isParticipate: false)
+
     // Group2
-    UserModel(userName: "Lunaa", userID: "", gender: "female", email: "sdfsd@naver.com", group: 2, target: ""),
-    UserModel(userName: "HeeHee", userID: "", gender: "female", email: "qweqw@naver.com", group: 2, target: ""),
-    UserModel(userName: "Ocean", userID: "", gender: "male", email: "ghj@naver.com", group: 2, target: ""),
-    UserModel(userName: "Goggins", userID: "", gender: "male", email: "elrkps@naver.com", group: 2, target: ""),
-    // Group3
-    UserModel(userName: "LuLu", userID: "", gender: "female", email: "sdlfks@naver.com", group: 3, target: ""),
-    UserModel(userName: "Hee", userID: "", gender: "female", email: "asla@naver.com", group: 3, target: ""),
-    UserModel(userName: "Suheon", userID: "", gender: "male", email: "qwleq@naver.com", group: 3, target: ""),
-    UserModel(userName: "Hoon", userID: "", gender: "male", email: "poask@naver.com", group: 3, target: ""),
-    // Group4
-    UserModel(userName: "Bokoo", userID: "", gender: "female", email: "sdlal@naver.com", group: 4, target: ""),
-    UserModel(userName: "Moo", userID: "", gender: "female", email: "relds@naver.com", group: 4, target: ""),
-    UserModel(userName: "SuuChoi", userID: "", gender: "male", email: "sdlfxp@naver.com", group: 4, target: ""),
-    UserModel(userName: "Yongs", userID: "", gender: "male", email: "asldk@naver.com", group: 4, target: "")
+//    UserModel(userName: "Lunaa", userID: "", gender: "female", email: "sdfsd@naver.com", group: 2, target: ""),
+//    UserModel(userName: "HeeHee", userID: "", gender: "female", email: "qweqw@naver.com", group: 2, target: ""),
+//    UserModel(userName: "Ocean", userID: "", gender: "male", email: "ghj@naver.com", group: 2, target: ""),
+//    UserModel(userName: "Goggins", userID: "", gender: "male", email: "elrkps@naver.com", group: 2, target: ""),
+//    // Group3
+//    UserModel(userName: "LuLu", userID: "", gender: "female", email: "sdlfks@naver.com", group: 3, target: ""),
+//    UserModel(userName: "Hee", userID: "", gender: "female", email: "asla@naver.com", group: 3, target: ""),
+//    UserModel(userName: "Suheon", userID: "", gender: "male", email: "qwleq@naver.com", group: 3, target: ""),
+//    UserModel(userName: "Hoon", userID: "", gender: "male", email: "poask@naver.com", group: 3, target: ""),
+//    // Group4
+//    UserModel(userName: "Bokoo", userID: "", gender: "female", email: "sdlal@naver.com", group: 4, target: ""),
+//    UserModel(userName: "Moo", userID: "", gender: "female", email: "relds@naver.com", group: 4, target: ""),
+//    UserModel(userName: "SuuChoi", userID: "", gender: "male", email: "sdlfxp@naver.com", group: 4, target: ""),
+//    UserModel(userName: "Yongs", userID: "", gender: "male", email: "asldk@naver.com", group: 4, target: "")
 ]
