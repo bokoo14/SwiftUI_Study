5e263cf41369d574a8b1b33c21105884931361d4
보경 <97583162+bokoo14@users.noreply.github.com>
2023-07-23 16:00:09 +0900

    [Feat] 헤더 드래그 제스쳐 적용

---
+++ NWebToon/NWebToon/Model/Header.swift
@@ -0,0 +1,14 @@
+//
+//  Header.swift
+//  NWebToon
+//
+//  Created by Bokyung on 2023/07/23.
+//
+
+import Foundation
+
+struct HeaderModel: Identifiable {
+    let id: UUID = UUID()
+    let imageTitle: String
+    let lineImageTitle: String
+}
--- NWebToon/NWebToon/View/Header/HeaderView.swift
+++ NWebToon/NWebToon/View/Header/HeaderView.swift
@@ -14,67 +14,68 @@ struct HeaderView: View {
     
     // index가 1일때는 왼쪽 16만큼 보임, 오른쪽 spacing 10, 오른쪽에 보이는 것 6
     // index가 2이상일때는 왼쪽 오른쪽 10만큼 spacing, 오른쪽 왼쪽에 보이는 부분 6
-    // 16 카드 10 6
-    // 6 10 카드 10 6
-    let leftrightSpacing: CGFloat = 10 // 배너 좌우 하나의 공백
-    let totalSpacing: CGFloat = 32 // 배너를 제외한 나머지의 공백
+    // index가 1일때: 16 카드 10 6
+    // index가 2이상일때: 6 10 카드 10 6
+    let leftrightSpacing: CGFloat = 10 // 배너 좌우 공백
+    let totalSpacing: CGFloat = 32 // 배너를 제외한 전체 공백
+    
+    let headerInfo: [HeaderModel] = {
+        var headerArray: [HeaderModel] = []
+        for index in 1..<9 {
+            let header = HeaderModel(imageTitle: "ImgBanner0\(index)", lineImageTitle: "ImgLineBanner0\(index)")
+            headerArray.append(header)
+        }
+        return headerArray
+    }()
+    
     var body: some View {
+        let screenWidth = UIScreen.main.bounds.width
+        let screeHeight = UIScreen.main.bounds.height
         
         GeometryReader { geo in
-            let width = geo.size.width // 전체 width
-            let adjustmentWidth = (width/2) - leftrightSpacing // 첫번째 index일때 왼쪽에 spacing을 추가적으로 더해줌 -> 결과값: 16
-            ZStack (alignment: .bottom){
-                HStack(spacing: 0){
-                    ForEach(1..<8) { index in
-                        Image("ImgBanner0"+"\(index)")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: geo.size.width) // 화면 전체를 차지
-                    } // ForEach
-                } // HStack
-                .offset(x: CGFloat(currentIndex)*(-width) + offset) // offset
-
-                HStack(spacing: 10){
-                    ForEach(1..<8) { index in
-                        Image("ImgLineBanner0"+"\(index)")
+            let width = geo.size.width - (totalSpacing - leftrightSpacing)
+            
+            Group {
+                ForEach(1..<headerInfo.count) { index in
+                    Image(headerInfo[index].imageTitle)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .opacity(index == currentIndex ? 1 : 0)
+                    //.animation(.linear, value: 1)
+                } // ForEach
+                
+                HStack(spacing: leftrightSpacing){
+                    ForEach(headerInfo) { headerinfo in
+                        Image(headerinfo.lineImageTitle)
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .frame(width: geo.size.width - totalSpacing) // 화면 전체 - 전체 공백
+                            .offset(x: CGFloat(currentIndex)*(-width) + offset, y: screeHeight/4.1)
                     } // ForEach
-                    Spacer()
+                    .animation(.easeInOut, value: offset == 0)
                 } // HStack
-                .offset(x: CGFloat(currentIndex)*(-width) + (currentIndex == 0 ? 16 : 38) + offset) // offset
-            } // ZStack
+                .padding(.horizontal, 16)
+                
+            } // Group
             .gesture(
                 DragGesture()
-                // updating: DragGesture를 하는 도중에 발생하는 event
-                //.updating(<#T##state: GestureState<State>##GestureState<State>#>, body: <#T##(DragGesture.Value, inout State, inout Transaction) -> Void#>)
-                // value에는 여러 값이 있고, 그 값을 활용 -> 예: translation과 같은 값을 가지고 있음
-                // inoutState의 값은 앞에 있는 state의 값과 동일한 변수
                     .updating($offset, body: { dragGestureValue, inoutState, inoutTransaction in
                         inoutState = dragGestureValue.translation.width
                     })
                 
-                // onChanged: 변화가 있으면 실행해라
-                // value: value.startLocation같은 많은 것들이 들어있다
-                    .onChanged({ value in })
-                
-                // 손을 뗐을때 실행됨
-                // 마지막 값을 갖고 싶을때
-                // value: value.startLocation같은 많은 것들이 들어있다
                     .onEnded { value in
-//                        // draw gesture로 변화된 width값
                         let widthOffset = value.translation.width
                         let howSwap = -widthOffset / width
                         let isNextIndex = howSwap.rounded()
-
+                        
                         currentIndex = max(min(currentIndex+Int(isNextIndex), 8), 0)
                     }
             ) // gesture
-
         } // GeometryReader
+        .border(.red)
+        .frame(width: screenWidth, height: screeHeight/3.4)
     }
-
+    
 }
 
 struct HeaderView_Previews: PreviewProvider {
