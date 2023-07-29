0ef630e3d983a504f374b2bf645f2e3e9e77385a
보경 <97583162+bokoo14@users.noreply.github.com>
2023-07-29 17:54:22 +0900

    [feat] custom font 적용

---
+++ NWebToon/NWebToon/Asset/assets/Fonts/FontManager.swift
@@ -0,0 +1,50 @@
+//
+//  FontManager.swift
+//  NWebToon
+//
+//  Created by Bokyung on 2023/07/29.
+//
+
+import SwiftUI
+
+extension Font {
+    enum Pretendard {
+        case black
+        case bold
+        case extrabold
+        case extralight
+        case light
+        case medium
+        case regular
+        case semibold
+        case thin
+        case custom(String)
+        
+        var value: String {
+            switch self {
+            case .black:
+                return "Pretendard-Black"
+            case .bold:
+                return "Pretendard-Bold"
+            case .extrabold:
+                return "Pretendard-ExtraBold"
+            case .extralight:
+                return "Pretendard-ExtraLight"
+            case .light:
+                return "Pretendard-Light"
+            case .medium:
+                return "Pretendard-Medium"
+            case .regular:
+                return "Pretendard-Regular"
+            case .semibold:
+                return "Pretendard-SemiBold"
+            case .thin:
+                return "Pretendard-Thin"
+            case .custom(let name):
+                return name
+            }
+        }
+    }
+    
+    
+}
